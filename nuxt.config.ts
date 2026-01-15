649a047d68f076357de460335888ed04e6c8fb78
HoangMaiLapTrinh <hoangmai020603@gmail.com>
2026-01-15 21:21:34 +0700

    update bootstrap

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -12,10 +12,11 @@ export default defineNuxtConfig({
     defaultLocale: 'en',
     strategy: 'prefix_except_default',
   },
+  css: [ 'bootstrap/dist/css/bootstrap.min.css' ],
   app: {
     head: {
       link: [
-        { rel: 'stylesheet', href: 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css', },
+        // { rel: 'stylesheet', href: 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css', },
         { rel: 'stylesheet', href: 'https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css' }
       ],
       script: [
