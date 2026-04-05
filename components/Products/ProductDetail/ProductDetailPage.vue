fa9f3c1f4a6e0ef35e2bab13404a50f397b8f8ec
HoangMaiLapTrinh <hoangmai020603@gmail.com>
2026-04-05 08:51:34 +0700

    test (chưaa check)

--- components/Products/ProductDetail/ProductDetailPage.vue
+++ components/Products/ProductDetail/ProductDetailPage.vue
@@ -1,74 +1,132 @@
 <template>
-  <section class="product-detail-page">
-    <div class="container-xl px-2 px-lg-3 py-3 py-lg-4">
-      <div class="row g-3 align-items-start">
-        <div class="col-12 col-xl-7">
-          <div class="content-stack">
-            <ProductDetailHeader :product="product" />
-            <ProductDetailGallery :product="product" />
-            <ProductDetailCommitments :items="product.commitments" />
-            <ProductDetailSpecs :specs="product.specs" />
-            <ProductDetailFaq :faqs="product.faqs" />
-          </div>
+    <section class="block-detail-product">
+        <div class="box-detail-product columns m-0">
+            <div class="box-detail-product__box-left column">
+                <ProductDetailLeft />
+            </div>
+            <div class="box-detail-product__box-center column">
+                <ProductDetailRight />
+            </div>
         </div>
-
-        <div class="col-12 col-xl-5">
-          <ProductDetailSidebar :product="product" />
+        <div>
+            <ProductSuggest />
         </div>
-      </div>
-
-      <div class="mt-3">
-        <ProductDetailRelated :products="product.relatedProducts" />
-      </div>
-
-      <ProductDetailContent
-        :article="product.articleSummary"
-        :news="product.productNews"
-        :video="product.reviewVideo"
-      />
-
-      <div class="content-stack mt-3">
-        <ProductDetailReviews
-          :summary="product.reviewSummary"
-          :reviews="product.reviews"
-        />
-        <ProductDetailQA :questions="product.questions" />
-      </div>
-    </div>
-
-    <ProductDetailStickyBar :product="product" />
-  </section>
+        <div class="block-content-product">
+            <ProductContentLeft />
+            <ProductContentRight />
+        </div>
+        <div class="boxReview">
+            <ProductBoxReview />
+        </div>
+        <div id="block-comment-cps" class="comment-container">
+            <ProductBlockComment />
+        </div>
+    </section>
 </template>
-
 <script setup>
-import ProductDetailCommitments from "@/components/Products/ProductDetail/ProductDetailCommitments.vue";
-import ProductDetailContent from "@/components/Products/ProductDetail/ProductDetailContent.vue";
-import ProductDetailFaq from "@/components/Products/ProductDetail/ProductDetailFaq.vue";
-import ProductDetailGallery from "@/components/Products/ProductDetail/ProductDetailGallery.vue";
-import ProductDetailHeader from "@/components/Products/ProductDetail/ProductDetailHeader.vue";
-import ProductDetailQA from "@/components/Products/ProductDetail/ProductDetailQA.vue";
-import ProductDetailRelated from "@/components/Products/ProductDetail/ProductDetailRelated.vue";
-import ProductDetailReviews from "@/components/Products/ProductDetail/ProductDetailReviews.vue";
-import ProductDetailSidebar from "@/components/Products/ProductDetail/ProductDetailSidebar.vue";
-import ProductDetailSpecs from "@/components/Products/ProductDetail/ProductDetailSpecs.vue";
-import ProductDetailStickyBar from "@/components/Products/ProductDetail/ProductDetailStickyBar.vue";
-
-defineProps({
-  product: {
-    type: Object,
-    required: true,
-  },
-});
+import ProductDetailLeft from '@/components/Products/ProductDetail/ProductDetailLeft.vue';
+import ProductDetailRight from '@/components/Products/ProductDetail/ProductDetailRight.vue';
+import ProductSuggest from '@/components/Products/ProductDetail/ProductSuggest.vue';
+import ProductContentLeft from '@/components/Products/ProductDetail/ProductContentLeft.vue';
+import ProductContentRight from '@/components/Products/ProductDetail/ProductContentRight.vue';
+import ProductBoxReview from '@/components/Products/ProductDetail/ProductBoxReview.vue';
+import ProductBlockComment from '@/components/Products/ProductDetail/ProductBlockComment.vue';
 </script>
-
 <style scoped>
-.product-detail-page {
-  min-height: 100%;
-  background: #f3f4f6;
+.block-detail-product {
+    padding-top: 10px;
+    position: relative;
 }
 
-.content-stack {
-  display: grid;
-  gap: 0.75rem;
+.block-detail-product .box-detail-product {
+    gap: 16px;
+    margin-bottom: 10px;
+    margin-top: 10px;
+}
+
+.block-detail-product .box-detail-product [class*=box-detail-product__box-] {
+    overflow-x: hidden;
+    padding: 0;
+}
+
+.block-detail-product .box-detail-product .box-detail-product__box-left {
+    height: -moz-max-content;
+    height: max-content;
+    position: sticky;
+    top: 88px;
+}
+
+.column {
+    display: block;
+    flex-basis: 0;
+    flex-grow: 1;
+    flex-shrink: 1;
+    padding: .75rem;
+}
+
+.block-detail-product .box-detail-product .box-detail-product__box-center {
+    height: -moz-max-content;
+    height: max-content;
+    position: sticky;
+    top: 88px;
+    width: 50%;
+    z-index: 10;
+}
+
+.block-detail-product .block-content-product {
+    display: flex;
+    gap: 24px;
+    justify-content: space-between;
+    margin: 10px auto;
+    position: relative;
+}
+
+.boxReview {
+    background-color: #f7f7f8;
+    border-radius: 16px;
+    overflow: hidden;
+    padding: 16px;
+    width: 100%;
+}
+
+.comment-container {
+    max-width: 100%;
+    overflow: hidden;
+}
+
+@media only screen and (min-width: 991px) and (max-width: 1199px) {
+    .block-detail-product .box-detail-product {
+        gap: 20px;
+        padding-top: 0;
+    }
+
+    .block-detail-product .block-content-product {
+        max-width: 100%;
+    }
+}
+
+@media only screen and (min-width: 769px) and (max-width: 990px) {
+    .block-detail-product {
+        padding-top: 0;
+    }
+
+    .block-detail-product .box-detail-product {
+        flex-wrap: wrap;
+        gap: 20px;
+        padding-top: 0;
+    }
+
+    .block-detail-product .box-detail-product .box-detail-product__box-left {
+        width: 50%;
+    }
+
+    .block-detail-product .box-detail-product .box-detail-product__box-center {
+        width: 50%;
+    }
+
+    .block-detail-product .block-content-product {
+        margin: auto;
+        max-width: 991px;
+    }
 }
 </style>
